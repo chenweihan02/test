8628c04dfc8fab79c9c05f9aa3a87d3f9a1f2099
Jle <1034558980@qq.com>
2025-08-07 10:34:46 +0800

    新增 01背包代码

--- test.cc
+++ test.cc
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+const int N = 1010;
+
+int n, m;
+int v[N], w[N];
+int f[N][N];
 
 int main()
 {
-  const std::string greeting = "Hello World!\n";
-  std::cout << greeting;
-  return 0;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i ++ ) cin >> v[i] >> w[i];
+    
+    for (int i = 1; i <= n; i ++ )
+        for (int j = 0; j <= m; j ++ )
+        {
+            f[i][j] = f[i - 1][j];
+            if (j >= v[i]) f[i][j] = max(f[i][j], f[i - 1][j - v[i]] + w[i]);
+        }
+    cout << f[n][m] << endl;
+    return 0;
 }
